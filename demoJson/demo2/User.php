39acb1ff97c33a2a6a092220ebfd8c71b52b74f3
nguyenngoc-it <83778951+nguyenngoc-it@users.noreply.github.com>
2021-06-15 17:33:16 +0700

    demojson

---
+++ demoJson/demo2/User.php
@@ -0,0 +1,47 @@
+<?php
+
+
+class User
+{
+    public $name;
+    public $password;
+
+    public function __construct($name, $password)
+    {
+        $this->name = $name;
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+
+
+
+}
